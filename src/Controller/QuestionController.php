779e5b5ca3d07ddc6f922813a866c950c39f8c51
Shanazar <snizamaddinow@gmail.com>
2023-05-24 14:24:23 +0300

    Bunch of files

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class QuestionController
 {
+    /**
+     * @Route("/homepage")
+     */
+    public function homepage(): Response
+    {
+        return new Response('Fuck you');
+    }
 
+    /**
+     * @Route("/show/{slug}")
+     */
+    public function show($slug): Response
+    {
+        $slug = ucwords(str_replace('-', ' ', $slug));
+        return new Response("Again fuck you ".$slug);
+    }
 }
