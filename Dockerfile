2f5eca9adaf1b2fa7712067bb88a7c616ca6643f
Shanazar Nizamaddinov <shanazar.nizamaddinov@bktomorrow.com>
2023-08-08 01:10:36 +0300

    updates to change votes dynamically.

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-#FROM php:7.4-fpm-alpine
-#RUN apk update && apk add curl && \
-#    curl -sS https://getcomposer.org/installer | php && \
-#    chmod +x composer.phar && mv composer.phar /usr/local/bin/composer
--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -2,29 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\AnswerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnswerController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
+     * @Route("/answers/{id}/vote", methods="POST", name="answer_vote")
      */
-    public function commentVote($id, $direction, LoggerInterface $logger)
+    public function answerVote($id, LoggerInterface $logger, Request $request, AnswerRepository $answerRepository, EntityManagerInterface $entityManager): JsonResponse
     {
-        // todo - use id to query the database
+        dump($id, $request);
+        $data = json_decode($request->getContent(), true);
+        $direction = $data['direction'] ?? 'up';
+
+        $answer = $answerRepository->find($id);
 
         // use real logic here to save this to the database
         if ($direction === 'up') {
             $logger->info('Voting up!');
-            $currentVoteCount = rand(7, 100);
+            $answer->voteUp();
         } else {
             $logger->info('Voting down!');
-            $currentVoteCount = rand(0, 5);
+            $answer->voteDown();
         }
 
-        return $this->json(['votes' => $currentVoteCount]);
+        $entityManager->flush();
+
+        return $this->json(['votes' => $answer->getVotesString()]);
     }
 }
