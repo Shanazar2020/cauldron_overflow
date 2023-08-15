04e45b3dadd41c209047444f96cdc6f0961f060e
Shanazar Nizamaddinov <shanazar.nizamaddinov@bktomorrow.com>
2023-08-15 17:35:58 +0300

    refactor: quote count restored

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,16 +13,18 @@ import $ from 'jquery';
 
 /**
  * Simple (ugly) code to handle the comment vote up/down
- */
-var $container = $('.js-vote-arrows');
-$container.find('button').on('click', function (e) {
-    e.preventDefault();
-    var $button = $(e.currentTarget);
+ //  */
+// var $container = $('.js-vote-arrows');
+// $container.find('button').on('click', function (e) {
+//     e.preventDefault();
+//     var $button = $(e.currentTarget);
+//
+//     $.ajax({
+//         url: '/comments/10/vote/' + $button.val(),
+//         method: 'POST'
+//     }).then(function (data) {
+//         $container.find('.js-vote-total').text(data.votes);
+//     });
+// });
 
-    $.ajax({
-        url: '/comments/10/vote/' + $button.val(),
-        method: 'POST'
-    }).then(function (data) {
-        $container.find('.js-vote-total').text(data.votes);
-    });
-});
+
