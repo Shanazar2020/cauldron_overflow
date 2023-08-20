78a45f0c41cc08ca602b484b5331e37a319320e5
Shanazar Nizamaddinov <shanazar.nizamaddinov@bktomorrow.com>
2023-08-21 00:01:36 +0300

    feature: fixture for tag and docker problem.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,8 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\Answer;
+use App\Entity\Tag;
 use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
+use App\Factory\TagFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,8 +14,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $tags = TagFactory::createMany(100);
 
-        $questions = QuestionFactory::new()->createMany(20);
+        $questions = QuestionFactory::new()->createMany(20, function () use ($tags) {
+            $random_tags = array();
+            for ($i = 0; $i < rand(1, 5); $i++) {
+                $random_tags[] = $tags[array_rand($tags)];
+            }
+            return ['tags' => $random_tags];
+        });
+
         QuestionFactory::new()->unpublished()->createMany(5);
 
         AnswerFactory::new()
