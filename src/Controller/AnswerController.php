b869fcf284cc83a97c074315a45ab48c07f65a22
Shanazar <snizamaddinow@gmail.com>
2023-09-18 17:56:28 +0300

    frontend modifications

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -6,18 +6,24 @@ use App\Entity\Answer;
 use App\Repository\AnswerRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AnswerController extends AbstractController
+class AnswerController extends BaseController
 {
     /**
      * @Route("/answers/{id}/vote", methods="POST", name="answer_vote")
+     * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
      */
     public function answerVote(Answer $answer, LoggerInterface $logger, Request $request, AnswerRepository $answerRepository, EntityManagerInterface $entityManager)
     {
-        $data = $request->request->all();
+        $logger->info("{user} is voting on answer {answer}", [
+            'user' => $this->getUser()->getEmail(),
+            'answer' => $answer->getId()
+        ]);
+        $data = json_decode($request->getContent(), true);
         $direction = $data['direction'] ?? 'up';
 
         // use real logic here to save this to the database
@@ -31,9 +37,7 @@ class AnswerController extends AbstractController
 
         $entityManager->flush();
 
-        return $this->redirectToRoute('app_question_show', [
-            'slug' => $answer->getQuestion()->getSlug()
-        ]);
+        return $this->json(['votes' => $answer->getVotes()]);
     }
 
     /**
