522c9b60a617d7c7f0e8ee744cae17d015e75b0c
Shanazar <snizamaddinow@gmail.com>
2023-09-19 15:32:00 +0300

    feature: email verified redirect to resend.

--- src/EventSubscriber/CheckVerifiedUserSubscriber.php
+++ src/EventSubscriber/CheckVerifiedUserSubscriber.php
@@ -3,18 +3,29 @@
 namespace App\EventSubscriber;
 
 use App\Entity\User;
+use App\Security\AccountNotVerifiedAuthenticationException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Http\Event\CheckPassportEvent;
+use Symfony\Component\Security\Http\Event\LoginFailureEvent;
 
 class CheckVerifiedUserSubscriber implements EventSubscriberInterface
 {
 
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
-            CheckPassportEvent::class => ['onCheckPassport', -10]
+            CheckPassportEvent::class => ['onCheckPassport', -10],
+            LoginFailureEvent::class => ['onLoginFailure']
         ];
     }
 
@@ -31,9 +42,19 @@ class CheckVerifiedUserSubscriber implements EventSubscriberInterface
         }
 
         if (!$user->getIsVerified()) {
-            throw new CustomUserMessageAuthenticationException(
-                'Please verify your account before logging in!'
-            );
+            throw new AccountNotVerifiedAuthenticationException();
         }
+    }
+
+    public function onLoginFailure(LoginFailureEvent $event)
+    {
+        if (!$event->getException() instanceof AccountNotVerifiedAuthenticationException) {
+            return;
+        }
+        $response = new RedirectResponse(
+            $this->router->generate('app_verify_resend_email')
+        );
+
+        $event->setResponse($response);
     }
 }
